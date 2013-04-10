d8c0efd8dc73f37d985689a0e931fceb03a4ed92
jaimin patel <jaimin9999@gmail.com>
2013-04-10 13:37:03 -0400

    random questions

--- script/widget.py
+++ script/widget.py
@@ -10,7 +10,7 @@ import json
 from HTMLParser import HTMLParser
 import re
 import logging
-#from random import choice
+from random import choice
 
 print "Content-Type: text/javascript\n"
 
@@ -37,9 +37,9 @@ def question_api_call(username,api_key):
 	headers = {'content-type': 'application/json', 'Accept': 'application/json', 'username':username, 'api_key':api_key}
 	
 	#temp code for demo
-	#offset_limit=[130,131,132,133]
-	#offset = choice(offset_limit)
-	url = 'http://tuvalabs.com/api/v1/widgetquestions/?limit=1'#%(offset)
+	offset_limit=[12,13,14,15]
+	offset = choice(offset_limit)
+	url = 'http://tuvalabs.com/api/v1/widgetquestions/?limit=1&offset=%s'%(offset)
 
 	data = {} 
 	result = {}
